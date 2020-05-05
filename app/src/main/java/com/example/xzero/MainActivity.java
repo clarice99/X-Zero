40f1b14cfea6dc86eccb48c3492a1a56c7fc7b8b
Clarice <claricedsilva99@gmail.com>
2020-05-05 22:21:11 +0530

    Shincan/Pingu function

--- app/src/main/java/com/example/xzero/MainActivity.java
+++ app/src/main/java/com/example/xzero/MainActivity.java
@@ -12,13 +12,13 @@ public class MainActivity extends AppCompatActivity {
 
     boolean gameactive =true;
     // Player representaion
-    // 0-X
-    // 1-0
+    // 0-Shinchan
+    // 1-Pingu
     int activeplayer = 0;
     int[] gamestate = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     // State meanings
-    // 0-X
-    // 1-0
+    // 0-shinchan
+    // 1-pingu
     // 2-Null
 
     int[][] winpositions = {{0,1,2}, {3,4,5},{6,7,8},
@@ -27,30 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void playertap(View view){
         ImageView img = (ImageView) view;
-        int tappedimage = Integer.parseInt(img.getTag().toString());
+        int tappedimg = Integer.parseInt(img.getTag().toString());
 
-        Log.i("Tap check", "img Tapped: "+ tappedimage);
+        Log.i("Tap check", "img Tapped: "+ tappedimg);
 
         if (!gameactive){
             gameReset(view);
             Log.i("reset  check", "Game has reset ");
         }
-        if (gamestate[tappedimage]==2 ){
-            gamestate[tappedimage]=activeplayer;
+        if (gamestate[tappedimg]==2 ){
+            gamestate[tappedimg]=activeplayer;
             img.setTranslationY(-1000f);
             if (activeplayer==0){
-                img.setImageResource(R.drawable.xalpha);
+                img.setImageResource(R.drawable.shinchan);
                 activeplayer=1;
                 TextView status = findViewById(R.id.msg);
-                Log.i("O check", "img print: "+ tappedimage);
-                status.setText("O's turn tap to play");
+                Log.i("Pingu check", "img print: "+ tappedimg);
+                status.setText("Pingu's turn tap to play");
             }
             else{
-                img.setImageResource(R.drawable.o);
+                img.setImageResource(R.drawable.pingu);
                 activeplayer=0;
                 TextView status = findViewById(R.id.msg);
-                Log.i("X check", "img print: "+ tappedimage);
-                status.setText("X's turn tap to play");
+                Log.i("Shinchan check", "img print: "+ tappedimg);
+                status.setText("Shinchan's turn tap to play");
             }
 
             img.animate().translationYBy(1000f).setDuration(300);
@@ -61,12 +61,12 @@ public class MainActivity extends AppCompatActivity {
                 String winnerstr;
                 gameactive= false;
                 if (gamestate[winposition[0]] == 0) {
-                    winnerstr = "X won!!";
+                    winnerstr = "Shinchan won!!";
                     TextView status = findViewById(R.id.msg);
                     status.setText(winnerstr);
 
                 } else {
-                    winnerstr = "O Won!!";
+                    winnerstr = "Pingu Won!!";
                     TextView status = findViewById(R.id.msg);
                     status.setText(winnerstr);
                 }
@@ -82,20 +82,21 @@ public class MainActivity extends AppCompatActivity {
         activeplayer = 0;
         for(int i=0; i<gamestate.length;i++){
             gamestate[i]=2;
-            ((ImageView)findViewById(R.id.imageView11)).setImageResource(0);
             ((ImageView)findViewById(R.id.imageView10)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView9)).setImageResource(0);
             ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
-            ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
-            ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
             ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
-            ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
-            ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
             ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
+            ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
 
             TextView status = findViewById(R.id.msg);
-            status.setText("X's turn tap to play");
+            status.setText("Shinchan's turn tap to play");
         }
     }
+    //hdchchgckhcjhchg
 
 
     @Override
